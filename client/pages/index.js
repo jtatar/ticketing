306c62b9b233a3a66c32e083a0c5aa5f92f651ef
Jakub Tatar <13kubek@gmail.com>
2021-01-22 16:39:29 +0100

    Fixed request to server

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,9 +6,22 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async () => {
-  const response = await axios.get('/api/users/currentuser');
-
-  return response.data;
+  if (typeof window === 'undefined') {
+    //server request
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+    return data;
+  } else {
+    //browser request
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
 };
 
 export default LandingPage;
