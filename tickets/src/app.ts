0935f2176a8a76c160a6695e35a07a45789d1129
Jakub Tatar <13kubek@gmail.com>
2021-01-24 18:25:22 +0100

    Added route in tickets + testing

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@jtattickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@jtattickets/common';
 import { createTicketRouter } from './routes/new';
 
 const app = express();
@@ -14,6 +14,7 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
 
 app.use(createTicketRouter);
 
