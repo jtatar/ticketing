1d12c9ddfd13afa1e2354edf3ad7a93dd1fab28b
Jakub Tatar <13kubek@gmail.com>
2021-02-02 17:29:08 +0100

    Created publishers for order, used them and created test for them

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -10,6 +10,8 @@ import { body } from 'express-validator';
 import mongoose from 'mongoose';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
+import { natsWrapper } from '../nats-wrapper';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
 
 const router = express.Router();
 const EXPIRATION_WINDOW_SECONDS = 15 * 60;
@@ -55,6 +57,16 @@ router.post(
     await order.save();
 
     //Publish an event saying that an order was created
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price,
+      },
+    });
 
     res.status(201).send(order);
   }
