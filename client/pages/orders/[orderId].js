534d1e64a91e15ae905d8519febd8df6734f2ee2
Jakub Tatar <13kubek@gmail.com>
2021-02-10 11:32:39 +0100

    Added pages for client functionality

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
 
-const OrderShow = ({ order }) => {
-  const [timeLeft, setTimeLeft] = useState('');
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => Router.push('/orders'),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -20,7 +31,18 @@ const OrderShow = ({ order }) => {
     return <div>Order Expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => doRequest({ token: token.id })}
+        stripeKey="pk_test_51IIYhmHVy5hhr507erYAgszVkK6jwDBpWxhBPkVEfdty647VIyxed7ZeT81zudrwvNcPbmZcrMYHpfyIhGuv1WST00scAWBC0u"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
